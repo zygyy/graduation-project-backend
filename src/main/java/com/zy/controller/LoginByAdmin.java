27696b2d163f1bdf2c5ed10d181d7c91bd6780ed
zygyy <1297799065@qq.com>
2020-02-29 12:19:59 +0800

    "解决员工信息的修改的bug"

--- src/main/java/com/zy/controller/LoginByAdmin.java
+++ src/main/java/com/zy/controller/LoginByAdmin.java
@@ -54,7 +54,7 @@ public class LoginByAdmin {
 
     @ApiOperation(value = "登录")
     @PostMapping("/login")
-    public RespBean login(@RequestBody LoginRequest loginRequest)throws Exception {
+    public RespBean login(@RequestBody LoginRequest loginRequest) throws Exception {
         return activateempService.login(loginRequest);
     }
 
@@ -68,21 +68,21 @@ public class LoginByAdmin {
     @PostMapping("/users")
     public RespBean getUsers(@RequestBody PaginationRequest paginationRequest) {
         //计算起始数据的下标并重新赋值
-        paginationRequest.setPagenum((paginationRequest.getPagenum()-1)*paginationRequest.getPagesize());
+        paginationRequest.setPagenum((paginationRequest.getPagenum() - 1) * paginationRequest.getPagesize());
         return employeeService.getEmployees(paginationRequest);
     }
 
 
     @ApiOperation(value = "删除离职员工")
     @DeleteMapping("/deleteUser/{id}")
-    public RespBean deleteUsers(@PathVariable long id, HttpServletRequest request){
+    public RespBean deleteUsers(@PathVariable long id, HttpServletRequest request) {
         Enumeration e = request.getHeaders("authorization");
         while (e.hasMoreElements()) {
             String headValue = (String) e.nextElement();
-            int result=employeeService.deleteEmployee(id,new Date(System.currentTimeMillis()),"原因不详！",jwtUtils.parseJwtTest(headValue),new Date(System.currentTimeMillis()));
-            if(result>0){
+            int result = employeeService.deleteEmployee(id, new Date(System.currentTimeMillis()), "原因不详！", jwtUtils.parseJwtTest(headValue), new Date(System.currentTimeMillis()));
+            if (result > 0) {
                 return RespBean.okMessage("删除员工成功!");
-            }else{
+            } else {
                 return RespBean.error("删除员工失败!");
             }
         }
@@ -92,11 +92,11 @@ public class LoginByAdmin {
 
     @ApiOperation(value = "根据员工号查找员工")
     @GetMapping("/getUserByEmpId/{empId}")
-    public RespBean getUserByEmpId(@PathVariable long empId){
-        Employee employee=employeeService.getEmployeeByEmpId(empId);
-        if(employee!=null){
-            return RespBean.ok("查询成功！",employee);
-        }else{
+    public RespBean getUserByEmpId(@PathVariable long empId) {
+        Employee employee = employeeService.getEmployeeByEmpId(empId);
+        if (employee != null) {
+            return RespBean.ok("查询成功！", employee);
+        } else {
             return RespBean.error("查询失败！");
         }
     }
@@ -110,25 +110,25 @@ public class LoginByAdmin {
 
     @ApiOperation(value = "修改员工信息或者删除员工")
     @PutMapping("/updateEmployee")
-    public RespBean updateOrDeleteEmployee(@RequestBody Employee employee,HttpServletRequest request){
+    public RespBean updateOrDeleteEmployee(@RequestBody Employee employee, HttpServletRequest request) {
         //解析更新的操作人
         Enumeration e = request.getHeaders("authorization");
         while (e.hasMoreElements()) {
             String headValue = (String) e.nextElement();
             employee.setUpdateUser(jwtUtils.parseJwtTest(headValue));
             employee.setUpdateTime(new Date(System.currentTimeMillis()));
-            if(employee.getLeaveDate()==null&&employee.getTerminationReason()==null){
-                int result=employeeService.updateEmployeeNotDelete(employee);
-                if(result>0){
+            if (employee.getLeaveDate() == null && employee.getTerminationReason() == null) {
+                int result = employeeService.updateEmployeeNotDelete(employee);
+                if (result > 0) {
                     return RespBean.okMessage("更新成功！");
-                }else{
+                } else {
                     return RespBean.error("更新失败！");
                 }
-            }else {
-                int result=employeeService.updateEmployeeAndDelete(employee);
-                if(result>0){
+            } else {
+                int result = employeeService.updateEmployeeAndDelete(employee);
+                if (result > 0) {
                     return RespBean.okMessage("删除成功！");
-                }else{
+                } else {
                     return RespBean.error("删除失败！");
                 }
             }
@@ -138,23 +138,23 @@ public class LoginByAdmin {
 
     @ApiOperation(value = "新增员工")
     @PostMapping("/addEmployee")
-    public RespBean addEmployee(@RequestBody AddEmployeerequest addEmployeerequest,HttpServletRequest request){
+    public RespBean addEmployee(@RequestBody AddEmployeerequest addEmployeerequest, HttpServletRequest request) {
         //解析创建的操作人
         Enumeration e = request.getHeaders("authorization");
         while (e.hasMoreElements()) {
             String headValue = (String) e.nextElement();
-            Long maxEmpId=employeeService.selectEmpId();
-            System.out.println("hello"+maxEmpId);
-            if(maxEmpId==null){
-                maxEmpId=new Long(1610701100);
+            Long maxEmpId = employeeService.selectEmpId();
+            System.out.println("hello" + maxEmpId);
+            if (maxEmpId == null) {
+                maxEmpId = new Long(1610701100);
             }
-            addEmployeerequest.setEmpId(maxEmpId+1);
+            addEmployeerequest.setEmpId(maxEmpId + 1);
             addEmployeerequest.setCreateUser(jwtUtils.parseJwtTest(headValue));
             addEmployeerequest.setCreateTime(new Date(System.currentTimeMillis()));
-            int result=employeeService.addEmployee(addEmployeerequest);
-            if(result>0){
+            int result = employeeService.addEmployee(addEmployeerequest);
+            if (result > 0) {
                 return RespBean.okMessage("创建新员工成功！");
-            }else{
+            } else {
                 return RespBean.error("创建失败！");
             }
         }
@@ -167,7 +167,7 @@ public class LoginByAdmin {
     public RespBean excel(@PathVariable String empIDs) throws Exception {
         //解析empIDs
         String[] excelEmpIDs = empIDs.split(",");
-        FileOutputStream fileOut = new FileOutputStream("d:\\学生成绩.xls");//文件输出位置
+        FileOutputStream fileOut = new FileOutputStream("d:\\员工信息.xls");//文件输出位置
         Workbook wb = new HSSFWorkbook();//定义新工作簿
         Sheet sheet = wb.createSheet("第一个sheet页");//构造sheet页
         CellStyle cellStyle = wb.createCellStyle();//定义日期单元格的格式
@@ -228,7 +228,7 @@ public class LoginByAdmin {
             row.createCell(1).setCellValue(employee.getChineseName());
             row.createCell(2).setCellValue(employee.getGender());
             row.createCell(3).setCellValue(employee.getGrade());
-            Cell cell4=row.createCell(4);
+            Cell cell4 = row.createCell(4);
             cell4.setCellValue(employee.getHireDate());
             cell4.setCellStyle(cellStyle);
             row.createCell(5).setCellValue(employee.getDepartment());
@@ -239,30 +239,30 @@ public class LoginByAdmin {
             row.createCell(10).setCellValue(employee.getSchool());
             row.createCell(11).setCellValue(employee.getMarjor());
             row.createCell(12).setCellValue(employee.getDegree());
-            Cell cell13=row.createCell(13);
+            Cell cell13 = row.createCell(13);
             cell13.setCellValue(employee.getGraduationTime());
             cell13.setCellStyle(cellStyle);
-            Cell cell14=row.createCell(14);
+            Cell cell14 = row.createCell(14);
             cell14.setCellStyle(cellStyle);
-            if(employee.getLeaveDate()!=null) {
+            if (employee.getLeaveDate() != null) {
                 cell14.setCellValue(employee.getLeaveDate());
-            }else{
+            } else {
                 cell14.setCellValue(" ");
             }
-            row.createCell(15).setCellValue(employee.getTerminationReason()!=null?employee.getTerminationReason():" ");
-            Cell cell16=row.createCell(16);
+            row.createCell(15).setCellValue(employee.getTerminationReason() != null ? employee.getTerminationReason() : " ");
+            Cell cell16 = row.createCell(16);
             cell16.setCellValue(employee.getBirthday());
             cell16.setCellStyle(cellStyle);
             row.createCell(17).setCellValue(employee.getCreateUser());
-            Cell cell18=row.createCell(18);
+            Cell cell18 = row.createCell(18);
             cell18.setCellValue(employee.getCreateTime());
             cell18.setCellStyle(cellStyle);
-            row.createCell(19).setCellValue(employee.getUpdateUser()!=null?employee.getUpdateUser():" ");
-            Cell cell20=row.createCell(20);
+            row.createCell(19).setCellValue(employee.getUpdateUser() != null ? employee.getUpdateUser() : " ");
+            Cell cell20 = row.createCell(20);
             cell20.setCellStyle(cellStyle);
-            if(employee.getUpdateTime()!=null) {
+            if (employee.getUpdateTime() != null) {
                 cell20.setCellValue(employee.getUpdateTime());
-            }else{
+            } else {
                 cell20.setCellValue(" ");
             }
             row.createCell(21).setCellValue(employee.getIsDeleted());
@@ -271,7 +271,6 @@ public class LoginByAdmin {
         fileOut.close();
         return RespBean.okMessage("导出成功！");
     }
-
 
 
 }
